9a93c36fc7e16c746b710c97208257e0aee162db
Joaquin Leklere <jleklere@yahoo.com.ar>
2024-05-12 22:59:21 -0300

    Progrss

--- ml_scraper_app/urls.py
+++ ml_scraper_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import scrape
+from .views import scrape, saveProduct
 
 urlpatterns = [
     path('scrape/<str:search_query>/<int:max_results>/', scrape, name='scrape'),
+    path('product/save', saveProduct, name='saveProduct'),
 ]
--- ml_scraper_app/views.py
+++ ml_scraper_app/views.py
@@ -1,9 +1,34 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .ml_scraper import scrape_mercado_libre
+import json
 
+@csrf_exempt
 def scrape(request, search_query, max_results=50):
     try:
         results = scrape_mercado_libre(search_query, int(max_results))
         return JsonResponse(results)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+@csrf_exempt
+def saveProduct(request):
+    try:
+        if request.method == "POST":
+            data = json.loads(request.body)
+            
+            nombre = data.get('nombre')
+            precio = data.get('precio')
+            url = data.get('url')
+            
+            product_info = f"Precio: ${precio}\nNombre: {nombre}\nLink: {url}\n\n"
+            
+            with open('savedProducts.txt', 'a', encoding='utf-8') as file:
+                file.write(product_info)
+            
+            return JsonResponse({'message': 'Producto guardado correctamente'}, status=200)
+        else:
+            return JsonResponse({'error': 'Método no permitido'}, status=405)
+            
     except Exception as e:
         return JsonResponse({'error': str(e)}, status=500)
