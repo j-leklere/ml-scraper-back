9662c3d57950caa7054195facef0bb8b8806ef72
j-leklere <jleklere@yahoo.com.ar>
2024-05-25 22:28:39 -0300

    USD API agregada, promedios, orden de resultados y precio x moneda agregados

--- ml_scraper_app/ml_scraper.py
+++ ml_scraper_app/ml_scraper.py
@@ -11,7 +11,7 @@ def get_page(url):
         print(f"HTTP Error: {e}")
         return None
 
-def parse_page(html):
+def parse_page(html, exchange_rate):
     """Extracts information from each product listing on the page."""
     if html is None:
         return []
@@ -30,8 +30,32 @@ def parse_page(html):
         currency = currency_element.text.replace('.', '').replace(',', '.') if currency_element else 'Moneda no disponible'
         link = link_element['href'] if link_element else 'Enlace no disponible'
 
-        results.append({'title': title, 'price': price,'currency': currency, 'link': link})
+        price = float(price)
+        price_usd = 0
+        price_ars = 0
+
+        if currency == "US$":
+            price_usd = price
+            price_ars = price * exchange_rate
+            currency = "USD"
+        elif currency == "$":
+            price_ars = price
+            price_usd = price / exchange_rate
+            currency = "ARS"
+
+        results.append({'title': title, 'price': price, 'priceArs': price_ars, 'priceUsd': price_usd, 'currency': currency, 'link': link})
     return results
+
+def fetch_exchange_rate():
+    try:
+        response = requests.get('https://dolarapi.com/v1/dolares/blue')
+        response.raise_for_status()
+        data = response.json()
+        promedio = (data['compra'] + data['venta']) / 2
+        return promedio
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP Error: {e}")
+        return 1
 
 def scrape_mercado_libre(search_query, max_results):
     base_url = "https://listado.mercadolibre.com.ar/"
@@ -39,6 +63,10 @@ def scrape_mercado_libre(search_query, max_results):
     results = []
     total_fetched = 0
     result_prices = []
+    result_prices_ars = []
+    result_prices_usd = []
+
+    exchange_rate = fetch_exchange_rate()
 
     while total_fetched < max_results:
         query_url = f"{base_url}{search_query.replace(' ', '-')}_Desde_{item_count}"
@@ -47,7 +75,7 @@ def scrape_mercado_libre(search_query, max_results):
             print("No more pages to fetch.")
             break
 
-        page_results = parse_page(html)
+        page_results = parse_page(html, exchange_rate)
         if not page_results:
             print("No more results found.")
             break
@@ -57,22 +85,38 @@ def scrape_mercado_libre(search_query, max_results):
                 break
             results.append(result)
             result_prices.append(float(result['price']))
+            result_prices_ars.append(float(result['priceArs']))
+            result_prices_usd.append(float(result['priceUsd']))
             total_fetched += 1
 
         item_count += 50
 
-    results = sorted(results, key=lambda x: x['price'])
+    results = sorted(results, key=lambda x: x['priceArs'])
     
-    if result_prices:
-        minimo = min(result_prices)
-        maximo = max(result_prices)
-        promedio = sum(result_prices) / total_fetched
+    if result_prices_ars:
+        minimo_ars = min(result_prices_ars)
+        maximo_ars = max(result_prices_ars)
+        promedio_ars = sum(result_prices_ars) / total_fetched
     else:
-        minimo = maximo = promedio = 0
+        minimo_ars = maximo_ars = promedio_ars = 0
+        
+    if result_prices_usd:
+        minimo_usd = min(result_prices_usd)
+        maximo_usd = max(result_prices_usd)
+        promedio_usd = sum(result_prices_usd) / total_fetched
+    else:
+        minimo_usd = maximo_usd = promedio_usd = 0
 
     return {
+        'ars': {
+            'minimo': minimo_ars,
+            'maximo': maximo_ars,
+            'promedio': promedio_ars
+        },
+        'usd': {
+            'minimo': minimo_usd,
+            'maximo': maximo_usd,
+            'promedio': promedio_usd
+        },
         'results': results,
-        'minimo': minimo,
-        'maximo': maximo,
-        'promedio': promedio
     }
